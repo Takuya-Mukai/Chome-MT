db9c5d3c9fb8c2706c836f6cf333bafefa81e82a
Kyle Chan <cwchanbh@connect.ust.hk>
2024-09-12 13:18:50 +0900

    update vis

--- script/ViscekSim.jl
+++ script/ViscekSim.jl
@@ -1,45 +1,45 @@
 include("../src/Chemo-MT.jl")
 
-# rho = 1
-# L = 10
-# J = 0.9
-# eta = 1
-# N = round(Int, L^2*rho)
-# alpha = eta - J*rho
+rho = 1
+L = 50
+J = 0.9
+eta = 1
+N = round(Int, L^2*rho)
+alpha = eta - J*rho
 
-# @show alpha
+@show alpha
 
-# paras = initParas(
-#                 Dr = eta, 
-#                 vel=1.0, 
-#                 J=J, 
-#                 epsilon=0.0, 
-#                 cutoff=2, 
-#                 L=L,
-#                 dx=0.1, 
-#                 sigma=1)
+paras = initParas(
+                Dr = eta, 
+                vel=1.0, 
+                J=J, 
+                epsilon=0.0, 
+                cutoff=2, 
+                L=L,
+                dx=0.1, 
+                sigma=1)
 
-# system = initSystem(paras, N=N)
+system = initSystem(paras, N=N)
 
-# @time all_pos, all_ori = simulation_visceks(system, paras; 
-#                                                     nsteps=50_000,
-#                                                     dt=0.001,
-#                                                     isave=60)
+@time all_pos, all_ori = simulation_visceks(system, paras; 
+                                                    nsteps=50_000,
+                                                    dt=0.001,
+                                                    isave=60)
 
 
 
-# @time Animattion(all_pos, all_ori, "Videos/Viscek/test_v1" * ".mp4"; framerate=60, L=paras.L)
+@time Animattion(all_pos, all_ori, "Videos/Viscek/test_v1" * ".mp4"; framerate=60, L=paras.L)
 
-# function get_order_parameter(all_ori)
-#     # sample = 1:100:10_000
-#     # n = length(sample)
-#     op_list = zeros(100)
-#     N = length(all_ori)
-#     for i in 1:100
-#         op_list[i] = norm(mean(angle2dir.(all_ori[N-i])))
-#     end
-#     return mean(op_list)
-# end
+function get_order_parameter(all_ori)
+    # sample = 1:100:10_000
+    # n = length(sample)
+    op_list = zeros(100)
+    N = length(all_ori)
+    for i in 1:100
+        op_list[i] = norm(mean(angle2dir.(all_ori[N-i])))
+    end
+    return mean(op_list)
+end
 
 function phase_transition(alpha_list)
 
@@ -86,63 +86,9 @@ function phase_transition(alpha_list)
 end
 
 
-# alpha_list = range(-2,1,40)
-# op_list = phase_transition(alpha_list)
-# data = Dict("op"=>op_list, "alpha" => alpha_list)
-# save("Data/viscek/phase_transistion_v2.jld2", data)
+alpha_list = range(-2,1,40)
+op_list = phase_transition(alpha_list)
+data = Dict("op"=>op_list, "alpha" => alpha_list)
+save("Data/viscek/phase_transistion_v2.jld2", data)
 
-
-
-"""
-sampling
-"""
-
-N_sample = 20
-
-function pt_sampling(N_sample)
-    for i in 1:N_sample
-        alpha_list = range(-1, 1, 40)
-        op_list = phase_transition(alpha_list)
-        data = Dict("op" => op_list, "alpha" => alpha_list)
-        save("Data/viscek/phase_transistion_v5_sample_$i.jld2", data)
-    end
-end
-
-@time pt_sampling(N_sample)
-
-data_dir = "Data/viscek/phase_transistion_v5_sample"
-function out_put_order(data_dir)
-    data = load(data_dir * "_1.jld2")
-    alpha = data["alpha"]
-    ops = [data["op"]]
-    for i in 2:10
-        dir = data_dir*"_$i.jld2"
-        data = load(data_dir * "_$i.jld2")
-        op = data["op"]
-        push!(ops, op)
-    end
-    return mean(ops)
-end
-
-op = out_put_order(data_dir)
-fig = Figure()
-ax = Axis(fig[1,1], xlabel="α", ylabel="|<v>|")
-scatter!(alpha,op)
-save("phase_transotion_v5.png", fig)
-
-# scatter(alpha_list, op_list)
-
-# println("ordre parameter = $(get_order_parameter(all_ori))")
-"""
-plot phase phase_transition
-"""
-
-# load("Data/viscek/phase_transistion_v2.jld2")
-# op = data["op"]
-# alpha = data["alpha"]
-
-# fig = Figure()
-# ax = Axis(fig[1,1], xlabel="α", ylabel="|<v>|")
-# scatter!(alpha,op)
-# save("phase_transotion_v2.png", fig)
 
--- src/vis.jl
+++ src/vis.jl
@@ -92,42 +92,45 @@ end
 
 
 function Animattion(pos, dir, spatio_func, out_filepath::AbstractString="test.mp4";
-    framerate::Integer=30, isave=1, L=1)
-
+    framerate::Integer=30, isave=1, L=1, backend=GLMakie)
 
     # fig, ax = wireframe(Sphere(Point3f(0), scale), linewidth=0.5, color=(:grey, 0.2))
-    fig = Figure()
-    ax = Axis(fig[1, 1])
-    ax.aspect = DataAspect()
-    limit = (-L, L, -L, L, -L, L)
+    fig = backend.Figure()
+    limit = (-L, L, -L, L)
+    ax = backend.Axis(fig[1, 1], xgridvisible=false, ygridvisible=false)
+    ax.aspect = backend.DataAspect()
+    # limits!(ax, limit)
+    ylims!(ax,0,L)
+    xlims!(ax, 0,L)
+
     XS = 0:L
     YS = 0:L 
 
-    field = [spatio_func(x,y, L) for x in 0:0.2:L, y in 0:0.2:L]
+    field = [spatio_func.(x,y, L) for x in 0:0.2:L, y in 0:0.2:L]
     #ax = Axis3(fig[1, 1], limits=limit, aspect=:data)
     # pos = Observable(Point2f.(first(rt)))
     # xs, ys = vec2comp.(pos)
-    x = Observable(vec2comp(pos[1])[1])
-    y = Observable(vec2comp(pos[1])[2])
+    x = backend.Observable(vec2comp(pos[1])[1])
+    y = backend.Observable(vec2comp(pos[1])[2])
     if dir[1] isa Vector{Float64}
         dir = [angle2dir.(ϕs) for ϕs in dir]
     end
-    u = Observable(vec2comp(dir[1])[1])
-    v = Observable(vec2comp(dir[1])[2])
+    u = backend.Observable(vec2comp(dir[1])[1])
+    v = backend.Observable(vec2comp(dir[1])[2])
 
     # pos = Observable(first(rt))
     # dir = Observable(first(nt))
 
     # strength = (sqrt.(u .^ 2 .+ v .^ 2))
-    C = Observable(field)
-    heatmap!(ax, XS, YS, C)
-    arrows!(ax, x, y, u, v,
+    C = backend.Observable(field)
+    backend.heatmap!(ax, XS, YS, C)
+    backend.arrows!(ax, x, y, u, v,
         linewidth=1, lengthscale=L / 40,
         align=:center)
 
     # scatter!(ax, pos)
 
-    CairoMakie.record(fig, out_filepath, eachindex(dir); framerate=framerate) do frame_i
+    backend.record(fig, out_filepath, eachindex(dir); framerate=framerate) do frame_i
         # pos[] = rt[frame_i]
         # dir[] = nt[frame_i]
         if frame_i % isave == 0
@@ -143,22 +146,30 @@ function Animattion(pos, dir, spatio_func, out_filepath::AbstractString="test.mp
 end
 
 
-function snapshort(pos, dir, field; backend=CairoMakie)
+function snapshort(pos, dir, spatio_func; backend=CairoMakie, colormap=:viridis,L=30)
     fig = backend.Figure()
-    ax = backend.Axis(fig[1, 1])
+    ax = backend.Axis(fig[1, 1], xgridvisible=false, ygridvisible=false)
     ax.aspect = backend.DataAspect()
-    limit = (-L, L, -L, L, -L, L)
-    XS = 0:L
-    YS = 0:L
+    ylims!(ax, 0, L)
+    xlims!(ax, 0, L)
+    XS = 0:0.2:L
+    YS = 0:0.2:L
+    field = [spatio_func.(x, y, L) for x in XS, y in YS]
+    x, y = vec2comp(pos)
+    if dir isa Vector{Float64}
+        dir = [angle2dir.(ϕs) for ϕs in dir]
+    end
+    u,v= vec2comp(dir)
+    # v = vec2comp(dir)
 
-    x = (vec2comp(pos)[1])
-    y = (vec2comp(pos)[2])
-    u = (vec2comp(dir)[1])
-    v = (vec2comp(dir)[2])
-    backend.heatmap!(ax, XS, YS, field)
+    # backend.heatmap!(ax, XS, YS, field, colormap=colormap, alpha=.1)
+    upb = 2L/3
+    lowb = L/2
+    backend.poly!(ax, Point2f[(0, lowb), (L, lowb), (L, upb), (0, upb)], color=(colormap, 0.5))
     backend.arrows!(ax, x, y, u, v,
         linewidth=1, lengthscale=L / 40,
         align=:center)
 
+    # hlines!([(2L / 3 - L / 2) / 2 + L / 2], linewidth=30, color=:blue)
     return fig
 end
