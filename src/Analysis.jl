06bcca4c0df711c7f28793eee54ddf55ddfc6c79
Kyle Chan <cwchanbh@connect.ust.hk>
2024-09-13 10:50:35 +0900

    add is_inside to analysis

--- src/Analysis.jl
+++ src/Analysis.jl
@@ -21,8 +21,33 @@ function get_XYorder_parameter(all_ori)
     return mean(abs.(x_list)), mean(abs.(y_list))
 end
 
+function is_inside_region(pos, xmin, xmax, ymin, ymax)
+    x, y = pos
+    return (xmin <= x <= xmax) && (ymin <= y <= ymax)
+end
 
 
+function get_order_parameter_inside(all_pos, all_ori, xmin, xmax, ymin, ymax)
+    op_list = zeros(10)
+    N = length(all_ori)
+    for i in 1:10
+        p_inside = all_ori[N-i][is_inside_region.(all_pos[N-i], xmin, xmax, ymin, ymax)]
+        op_list[i] = norm(mean(angle2dir.(p_inside)))
+    end
+    return mean(op_list)
+end
+
+function get_XYorder_parameter_inside(all_pos, all_ori, xmin, xmax, ymin, ymax)
+    x_list = zeros(10)
+    y_list = zeros(10)
+    N = length(all_ori)
+    # Np = length(all_ori[end])
+    for i in 1:10
+        p_inside = all_ori[N-i][is_inside_region.(all_pos[N-i], xmin, xmax, ymin, ymax)]
+        x_list[i], y_list[i] = mean(angle2dir.(p_inside))
+    end
+    return mean(abs.(x_list)), mean(abs.(y_list))
+end
 
 
 
